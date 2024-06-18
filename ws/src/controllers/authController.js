c4d43aaa9f1c6bfb776e43282fe830bfffac5c37
Gustavo de Oliveira Neves <chips_gustavo2@outlook.com>
2024-06-18 13:57:01 -0300

    Ajuste na resposta HTTP

--- ws/src/controllers/authController.js
+++ ws/src/controllers/authController.js
@@ -1,58 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const Estabelecimento = require('../models/estabelecimento');
-const bcrypt = require('bcryptjs');
+const Estabelecimento = require('caminho/para/modelo/Estabelecimento');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.json')
+const bcrypt = require('bcryptjs');
+const authConfig = require('caminho/para/authConfig');
 
-router.post('/register', async (req,res) =>{
+router.post('/register', async (req, res) => {
     try {
         const existingUser = await Estabelecimento.findOne({ email: req.body.email });
         if (existingUser) {
-                res.json({ error:'E-mail já está em uso' });
+            return res.json({ error: 'E-mail já está em uso' }); 
         }
-        const User = await new Estabelecimento(req.body).save();
-        User.senha = undefined;
-        const token=jwt.sign({
-            id:User._id,         
-        }, authConfig.secret ,{
-            expiresIn: 86400
-        });     
-        return res.json({user:{id:User._id,nome:User.nome},token});
-    } catch(err){
-        res.json({error:'Todos os campos do registro são obrigatorios!'})
-    }
-});
-
-router.post('/auth', async(req,res) => {
-    try {
-        const {email,senha} = req.body;
-        const user = await Estabelecimento.findOne({ email }).select("+senha");
-        if (!user) {
-            return res.json({
-                error: "Email nao encontrado",           
-            });
-        }
-        if (!await bcrypt.compare(senha, user.senha)) {
-            return res.json({
-                error: "Senha inválida"  
-            });
-        }
-        user.senha = undefined;
-        const token=jwt.sign({
-            id:user._id,      
-        }, authConfig.secret ,{
+        const newUser = await new Estabelecimento(req.body).save();
+        newUser.senha = undefined;
+        const token = jwt.sign({
+            id: newUser._id,
+        }, authConfig.secret, {
             expiresIn: 86400
         });
-      
-        return res.json({user:{id:user._id,nome:user.nome},token});  
-    } catch(err) {
-        res.json({
-            error:"Erro no servidor, entre em contato com suporte!"
-        })
+        return res.json({ user: { id: newUser._id, nome: newUser.nome }, token });
+    } catch (err) {
+        return res.json({ error: 'Todos os campos do registro são obrigatórios!' });  
     }
-    
 });
 
+router.post('/auth', async (req, res) => {
+    try {
+        const { email, senha } = req.body;
+        const user = await Estabelecimento.findOne({ email }).select("+senha");
+        if (!user) {
+            return res.json({ error: "Email não encontrado" }); 
+        }
+        if (!await bcrypt.compare(senha, user.senha)) {
+            return res.json({ error: "Senha inválida" });  
+        }
+        user.senha = undefined;
+        const token = jwt.sign({
+            id: user._id,
+        }, authConfig.secret, {
+            expiresIn: 86400
+        });
+        return res.json({ user: { id: user._id, nome: user.nome }, token });
+    } catch (err) {
+        return res.json({ error: "Erro no servidor, entre em contato com suporte!" });  
+    }
+});
 
 module.exports = router;
